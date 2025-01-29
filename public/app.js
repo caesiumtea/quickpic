97bfe544f5926b78abfa3306a4e164ee18a8b6ae
vance <caesiumtea@gmail.com>
2025-01-28 21:57:19 -0500

    Fix img width and add comments to app.js

--- public/app.js
+++ public/app.js
@@ -4,35 +4,30 @@ let interval;
 
 let timerCircle = document.getElementById("timer-circle");
 let photo = document.getElementById("photo");
-// function endTimer() {
-//     countdownOn = false;
-//     console.log("done")
-//     timerCircle.innerText = "start";
-// }
 
+// Decrements the timer when called by `interval` (in variable and on screen)
+// Also checks whether the timer is out, and resets it if so
 function updateTime() {
     if (time === 0) {
         clearInterval(interval);
         countdownOn = false;
-        console.log("done")
         timerCircle.innerText = "start";
     } else {
         time -= 1;
         timerCircle.innerText = time;
     }
-    // console.log(time);
 }
 
+// Gets new image and starts the timer by creating an Interval
 function startTimer() {
     // photo refresh code from https://stackoverflow.com/questions/1077041/refresh-image-with-a-new-one-at-the-same-url
     photo.src = "https://picsum.photos/500?t=" + new Date().getTime();
     time = maxTime;
     timerCircle.innerHTML = time;
-    interval = setInterval(updateTime, 1000);
+    interval = setInterval(updateTime, 1000); //updates every 1 second
 }
 
-
-
+// Calls startTimer when circle is clicked
 timerCircle.addEventListener("click", function () {
     if (!countdownOn) {
         countdownOn = true;
--- readme.md
+++ readme.md
@@ -6,8 +6,11 @@ Created for Codedex.io JS course final project.
 
 ## To do
 - Address problem where the timer starts counting down while the new image is still loading, so that you don't actually get the full 30 seconds to draw
+- Use a more semantic element instead of just adding a listener to a div
 - Add sound when timer finishes
 - Make it possible to change the timer length (preset choices, or enter your own number?)
 - Improve the text styling 
-- Use Unsplash instead of Picsum
-- Make the sizes of image, button, and text within button responsive
+- Make button/timer float on the side or something so that it remains on screen alongside image on short wide screens?
+- Give the circle a visual countdown ring that shrinks with the time remaining
+- Use Unsplash or similar (higher quality photos) instead of Picsum
+- Allow user to enter a search term instead of image being completely random
